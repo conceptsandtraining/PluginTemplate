f3fd488132a39d07e415be6d9bc871a3e607bcb2
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-10-26 15:49:51 +0200

    fill plugin.php with required functions

--- PLUGINNAME/classes/class.il<PLUGINNAME>Plugin.php
+++ PLUGINNAME/classes/class.il<PLUGINNAME>Plugin.php
@@ -1,10 +1,10 @@
 <?php
-include_once("./Services/Repository/classes/class.ilRepositoryObjectPlugin.php");
+require_once('./Modules/OrgUnit/classes/Types/class.ilOrgUnitTypeHookPlugin.php');
 
 /**
  * Plugin base class. Keeps all information the plugin needs
  */
-class il<PLUGINNAME>Plugin.php extends ilRepositoryObjectPlugin {
+class il<PLUGINNAME>Plugin.php extends ilOrgUnitTypeHookPlugin {
 	/**
 	 * Get the name of the Plugin
 	 *
@@ -15,8 +15,65 @@ class il<PLUGINNAME>Plugin.php extends ilRepositoryObjectPlugin {
 	}
 
 	/**
-	 * Defines custom uninstall action like delete table or something else
-	 */
-	protected function uninstallCustom() {
+	* Return false if setting a title is not allowed
+	*
+	* @inhertidoc
+	*/
+	public function allowSetTitle($a_type_id, $a_lang_code, $a_title) {
+		return (true || false);
+	}
+
+	/**
+	* Return false if setting a description is not allowed
+	*
+	* @inhertidoc
+	*/
+	public function allowSetDescription($a_type_id, $a_lang_code, $a_description) {
+		return (true || false);
+	}
+
+	/**
+	* Return false if setting a default language is not allowed
+	*
+	* @inhertidoc
+	*/
+	public function allowSetDefaultLanguage($a_type_id, $a_lang_code) {
+		return (true || false);
+	}
+
+	/**
+	* Return false if OrgUnit type cannot be deleted
+	*
+	* @inhertidoc
+	*/
+	public function allowDelete($a_type_id) {
+		return (true || false);
+	}
+
+	/**
+	* Return false if OrgUnit type is locked and no updates are possible
+	*
+	* @inhertidoc
+	*/
+	public function allowUpdate($a_type_id) {
+		return (true || false);
+	}
+
+	/**
+	* Return false if an AdvancedMDRecord cannot be assigned to an OrgUnit type
+	*
+	* @inhertidoc
+	*/
+	public function allowAssignAdvancedMDRecord($a_type_id, $a_record_id) {
+		return (true || false);
+	}
+
+	/**
+	* Return false if an AdvancedMDRecord cannot be deassigned from an OrgUnit type
+	*
+	* @inhertidoc
+	*/
+	public function allowDeassignAdvancedMDRecord($a_type_id, $a_record_id) {
+		return (true || false);
 	}
 }
