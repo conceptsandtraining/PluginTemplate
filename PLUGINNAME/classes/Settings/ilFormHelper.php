0875e319c61914380f0bf5378541c1ba2acf7e05
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-10-26 14:20:29 +0200

    remove deprecated files

--- PLUGINNAME/classes/Settings/ilFormHelper.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * This trait defines functions you need to create form elements or values
- * for the settings. E.g. the Object has additional setting "OrgUnit".
- */
-trait ilFormHelper {
-
-}
