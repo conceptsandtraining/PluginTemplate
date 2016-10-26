db3583e8cd46ce51445514c164ef504a0b230de6
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-10-26 15:21:03 +0200

    cleanup and correct plugin class

--- PLUGINNAME/classes/class.il<PLUGINNAME>Plugin.php
+++ PLUGINNAME/classes/class.il<PLUGINNAME>Plugin.php
@@ -1,22 +1,24 @@
 <?php
-include_once("./Services/Repository/classes/class.ilRepositoryObjectPlugin.php");
+require_once("Services/AdvancedMetaData/classes/class.ilAdvancedMDClaimingPlugin.php");
 
 /**
  * Plugin base class. Keeps all information the plugin needs
  */
-class il<PLUGINNAME>Plugin.php extends ilRepositoryObjectPlugin {
+class il<PLUGINNAME>Plugin extends ilAdvancedMDClaimingPlugin {
 	/**
 	 * Get the name of the Plugin
 	 *
 	 * @return string
 	 */
-	function getPluginName() {
-		return "<PLUGINNAME>";
+	public function getPluginName() {
+		return "PLUGINNAME";
 	}
-
 	/**
-	 * Defines custom uninstall action like delete table or something else
+	 * Check the permissions
+	 *
+	 * @inheritdoc
 	 */
-	protected function uninstallCustom() {
+	public function checkPermission($a_user_id, $a_context_type, $a_context_id, $a_action_id, $a_action_sub_id) {
+		
 	}
 }
