93e2e6f8074f6a8ed6bbda8fdc6a043093759e85
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-10-26 15:00:56 +0200

    change pluginpath in run test

--- PLUGINNAME/run_tests.sh
+++ PLUGINNAME/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #plugin path from ilias-root:
-PLUGINPATH='Customizing/global/plugins/Services/Repository/RepositoryObject/Jill';
+PLUGINPATH='Customizing/global/plugins/Services/Repository/RepositoryObject/PLUGINNAME';
 SCRIPT_PATH=$(dirname "$0");
 cd $SCRIPT_PATH;
 
