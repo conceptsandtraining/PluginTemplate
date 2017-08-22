e598be97cba4618b04c91393d998a43de5c35d28
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2017-08-22 11:37:52 +0200

    New default funktion to get the parent course

--- classes/class.ilObj<PLUGINNAME>.php
+++ classes/class.ilObj<PLUGINNAME>.php
@@ -42,4 +42,27 @@ class ilObj<PLUGINNAME> extends ilObjectPlugin implements Obj<PLUGINNAME> {
 	public function doCloneObject($new_obj, $a_target_id, $a_copy_id = null) {
 
 	}
+
+	/**
+	 * Get the parent course
+	 *
+	 * @return ilObjCourse | null
+	 */
+	public function getParentCourse()
+	{
+		global $DIC;
+		$tree = $DIC->repositoryTree();
+		$parents = $tree->getPathFull($this->getRefId());
+		$parent = array_filter($parents, function($p) {
+			if($p["type"] == "crs") {
+				return $p;
+			}
+		});
+		if(count($parent) > 0) {
+			$parent_crs = array_shift($parent);
+			require_once("Services/Object/classes/class.ilObjectFactory.php");
+			return ilObjectFactory::getInstanceByRefId($parent_crs["ref_id"]);
+		}
+		return null;
+	}
 }
