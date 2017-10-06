b3a73dae14d02d127cd2d13a1540636163f6abcd
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2017-10-06 13:55:14 +0200

    Remove file extension from class name

--- classes/class.il<PLUGINNAME>Plugin.php
+++ classes/class.il<PLUGINNAME>Plugin.php
@@ -4,7 +4,7 @@ include_once("./Services/Repository/classes/class.ilUserInterfaceHookPlugin.php"
 /**
  * Plugin base class. Keeps all information the plugin needs
  */
-class il<PLUGINNAME>Plugin.php extends ilUserInterfaceHookPlugin {
+class il<PLUGINNAME>Plugin extends ilUserInterfaceHookPlugin {
 	/**
 	 * Get the name of the Plugin
 	 *
