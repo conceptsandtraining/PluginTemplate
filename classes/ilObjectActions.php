089441437f25c924bec01961b5af31149c5194d4
daniwe4 <daniel.weise@concepts-and-training.de>
2018-02-01 08:13:38 +0000

    rename to object actions and add a namespce

---
+++ classes/ilObjectActions.php
@@ -0,0 +1,11 @@
+<?php
+namespace CaT\Plugins\{{PLUGINNAME}};
+
+/**
+ * Communication class between front- and backend.
+ * E.g. GUI only use this class to get information from ILIAS DB.
+ */
+class ilObjectActions
+{
+
+}
