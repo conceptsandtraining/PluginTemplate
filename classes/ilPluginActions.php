842c073a38cbf922449ee59a87edbeb870dc2264
daniwe4 <daniel.weise@concepts-and-training.de>
2018-02-01 08:15:15 +0000

    add file ilPluginActions

---
+++ classes/ilPluginActions.php
@@ -0,0 +1,11 @@
+<?php
+namespace CaT\Plugins\{{PLUGINNAME}};
+
+/**
+ * Communication class between front- and backend if there exists a configuration GUI.
+ * E.g. GUI only use this class to get information from ILIAS DB.
+ */
+class ilPluginActions
+{
+
+}
