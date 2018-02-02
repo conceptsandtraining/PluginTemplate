e8bc31fc29940ef05bcbfa38e5d9ac07c4b20e71
daniwe4 <daniel.weise@concepts-and-training.de>
2018-02-02 13:08:09 +0000

    implement skeleton for plugin actions

--- classes/ilPluginActions.php
+++ classes/ilPluginActions.php
@@ -1,9 +1,12 @@
 <?php
-namespace CaT\Plugins\{{PLUGINNAME}};
+namespace CaT\Plugins\<PLUGINNAME>;
 
 /**
  * Communication class between front- and backend if there exists a configuration GUI.
  * E.g. GUI only use this class to get information from ILIAS DB.
+ *
+ * @author
+ * @copyright Extended GPL, see LICENSE
  */
 class ilPluginActions
 {
