23781420b3a7fda5bfd6d4e199831cadf4760ae7
daniwe4 <daniel.weise@concepts-and-training.de>
2018-02-02 13:15:01 +0000

    corect placeholder and add docstring for author and license

--- classes/Settings/<PLUGINNAME>.php
+++ classes/Settings/<PLUGINNAME>.php
@@ -1,8 +1,11 @@
 <?php
-namespace CaT\Plugins\{{PLUGINNAME}}\Settings;
+namespace CaT\Plugins\<PLUGINNAME>\Settings;
 
 /**
  * This is the object for additional settings.
+ *
+ * @author
+ * @copyright Extended GPL, see LICENSE
  */
 class <PLUGINNAME>
 {
